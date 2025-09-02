61e1708e7cba540c63b78f94c6a7fb1f9a7a655b
Sharif Derhem <sharifderhem@gmail.com>
2025-09-02 20:22:19 +0300

    Update report_generator.py

--- smart_reports/report_generation/report_generator.py
+++ smart_reports/report_generation/report_generator.py
@@ -397,7 +397,7 @@ def generate_rankings_dict(sites: List[Dict], top_n: int, MAX_TOTAL: float, CRIT
             CRITERION_WEIGHTS['demographics']
         )
         competitive_100 = normalize_score_to_100(
-            site.get('scores', {}).get('competitive_score', 0), 
+            site.get('scores', {}).get('competition_score', 0), 
             CRITERION_WEIGHTS['competition']
         )
         healthcare_100 = normalize_score_to_100(
